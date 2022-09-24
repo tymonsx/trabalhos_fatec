411fb8110ea95d94109d1070ce2c34d6cb54c842
tymonsx <tymonsx@hotmail.comm>
2022-09-24 17:14:43 -0300

    ultimos trabalhos entregues da tarde

---
+++ alunos/1680482122021/index.js
@@ -0,0 +1,15 @@
+function chamaMenu(){
+  let link = document.getElementById("Links2")
+  link.style.display = "block"
+
+}
+
+function fechar(){
+  let link = document.getElementById("Links2")
+  link.style.display = "none"
+}
+
+function irParaSessao(){
+  let link = document.getElementById("Links2")
+  link.style.display = "none"
+}
---
+++ alunos/1680482122023/musicas.js
@@ -0,0 +1,9 @@
+function menu(){
+	let menu = document.getElementById("menuAux");
+	menu.style.display = "block"
+}
+
+function fechar(){
+	let menu = document.getElementById("menuAux");
+	menu.style.display = "none"
+}
